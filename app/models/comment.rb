024e0556e9087fe0f00b18f45f7adaa1550329fb
Nikita Nyatin <yesnik@yandex.ru>
2016-10-26 22:43:56 +0500

    Add comment for polymorphic association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  # Rails thinks that Commentable model exists. In 'comments' table
+  # we created fields: :commentable_type, :commentable_id
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 end
